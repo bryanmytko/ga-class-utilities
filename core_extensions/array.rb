3e9fae516eb15aa9ab35f31fb12856d7e7ce7815
Bryan Mytko <bryanmytko@gmail.com>
2016-07-05 15:45:28 -0400

    Add core extensions

---
+++ core_extensions/array.rb
@@ -0,0 +1,19 @@
+module CoreExtensions
+  class Array
+    def chunk(pieces)
+      debugger
+      pieces ||= 2
+      len = self.length
+      mid = (len / pieces)
+      chunks = []
+      start = 0
+      1.upto(pieces) do |i|
+        last = start+mid
+        last = last - 1 unless len % pieces >= i
+        chunks << self[start..last] || []
+        start = last + 1
+      end
+      chunks
+    end
+  end
+end
