60d70b1cf073b72348c003eeba253b78aec033c4
Bryan Mytko <bryanmytko@gmail.com>
2016-07-06 09:51:52 -0400

    Add even groups unit test

--- test/test_even_groups.rb
+++ test/test_even_groups.rb
@@ -1,12 +1,22 @@
 require "minitest/autorun"
 require "./student_util"
+require "byebug"
 
 class TestMinitestStudentUtil < Minitest::Test
   def setup
-    @student_util = StudentUtil.new
+    students = "test/data/students.json"
+    instructors = "test/data/instructors.json"
+    student_util = StudentUtil.new(students, instructors)
+
+    @even_groups = student_util.even_groups_per_instructor
   end
 
   def test_that_groups_per_instructor_can_be_made
-    assert_instance_of Array, @student_util.even_groups_per_instructor
+    assert_instance_of Array, @even_groups
+  end
+
+  def test_that_groups_per_instructor_are_near_even
+    diff = (@even_groups[1][1].length - @even_groups[0][1].length)
+    assert_includes 0..1, diff.abs
   end
 end
